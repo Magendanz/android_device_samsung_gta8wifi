a52d5121fcba57f04fcba76dbdac26feb19ff615
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2023-08-05 22:45:27 +0100

    Add files via upload

--- device.mk
+++ device.mk
@@ -5,11 +5,23 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+PRODUCT_PLATFORM := ums512
+
 LOCAL_PATH := device/samsung/gta8wifi
 
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.0-impl-wrapper.recovery \
+    android.hardware.boot@1.0-impl-wrapper \
+    android.hardware.boot@1.0-impl.recovery \
+    bootctrl.$(PRODUCT_PLATFORM) \
+    bootctrl.$(PRODUCT_PLATFORM).recovery \
+
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
