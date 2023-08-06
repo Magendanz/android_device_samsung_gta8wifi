e9b328bd94fc1eeb9ba048f6e56adf9f2a2ea4d8
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2023-08-06 08:16:26 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_gta8wifi-user
-add_lunch_combo omni_gta8wifi-userdebug
-add_lunch_combo omni_gta8wifi-eng
+add_lunch_combo twrp_gta8wifi-user
+add_lunch_combo twrp_gta8wifi-userdebug
+add_lunch_combo twrp_gta8wifi-eng
